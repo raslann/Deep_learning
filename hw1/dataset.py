3626024ee079df6d4511f9063c2f2550a97ce344
Gan Quan <coin2028@hotmail.com>
2017-03-04 03:15:25 -0500

    convolutional ladder network.  ladder.py is now obsolete

--- hw1/dataset.py
+++ hw1/dataset.py
@@ -10,6 +10,8 @@ with open('train_unlabeled.p', 'rb') as f:
     train_unlabeled = pickle.load(f)
 with open('validation.p', 'rb') as f:
     valid = pickle.load(f)
+with open('test.p', 'rb') as f:
+    test = pickle.load(f)
 
 train_labeled_data = train_labeled.train_data
 train_unlabeled_data = train_unlabeled.train_data
@@ -22,10 +24,12 @@ labeled_dataset = TensorDataset(train_labeled_data, train_labeled_labels)
 unlabeled_dataset = TensorDataset(train_unlabeled_data, train_unlabeled_labels)
 train_dataset = TensorDataset(train_data, train_labels)
 valid_dataset = TensorDataset(valid.test_data, valid.test_labels)
+test_dataset = TensorDataset(test.test_data, test.test_labels)
 labeled_loader = DataLoader(labeled_dataset, 100, True)
 unlabeled_loader = DataLoader(unlabeled_dataset, 100, True)
 train_loader = DataLoader(train_dataset, 100, True)
 valid_loader = DataLoader(valid_dataset, 100, True)
+test_loader = DataLoader(test_dataset, 1, False)
 
 labeled_gen = iter(labeled_loader)
 unlabeled_gen = iter(unlabeled_loader)
--- hw1/util.py
+++ hw1/util.py
@@ -1,15 +1,68 @@
 
 import torch as T
 from torch.autograd import Variable
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--unlabeled",
+                    help="use unlabeled data",
+                    action="store_true")
+parser.add_argument("--modelname",
+                    help="model name",
+                    type=str)
+parser.add_argument("--loadmodel",
+                    help="file to load model from for continuation",
+                    type=str)
+parser.add_argument("--lambdas",
+                    help="file to load lambdas from",
+                    type=str)
+parser.add_argument("--cuda",
+                    help="use cuda",
+                    action="store_true")
+parser.add_argument("--ensemble",
+                    help="number of models",
+                    type=int)
+parser.add_argument("--gamma",
+                    help="gamma model",
+                    action="store_true")
+
+args = parser.parse_args()
+
+def variable(*args_, **kwargs):
+    if args.cuda:
+        return Variable(*args_, **kwargs).cuda()
+    else:
+        return Variable(*args_, **kwargs)
+
+def var_to_numpy(v):
+    if args.cuda:
+        return v.cpu().data.numpy()
+    else:
+        return v.data.numpy()
 
 def alloc_list(n):
     return [None] * n
 
 def anynan(t):
-    return (t != t).sum() > 0
+    return ((t != t).sum() > 0) or ((t.abs() > 1e+7).sum() > 0)
 
 def noise(size, scale=0.3, center=0):
-    return Variable(T.randn(*size)) * scale + center
+    return variable(T.randn(*size)) * scale + center
 
 def noised(x, scale=0.3, center=0):
-    return x + Variable(T.randn(*x.size())) * scale + center
+    return x + variable(T.randn(*x.size())) * scale + center
+
+def batchnorm_mean_var(x):
+    ndim = x.dim()
+    batch_size = x.size()[0]
+
+    if ndim == 4:
+        # Torch does not have mean/sum/variance over multiple axes...
+        mean = x.mean(0).mean(2).mean(3)
+        diff = x - mean.expand_as(x)
+        var = (diff ** 2).mean(0).mean(2).mean(3)
+    else:
+        mean = x.mean(0)
+        var = x.var(0) * (batch_size - 1) / batch_size
+
+    return mean, var
